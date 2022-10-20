7695c5286afccbaa4395f65a8ae3f644f6f00d73
akashbhatkar <114064348+akashbhatkar@users.noreply.github.com>
2022-10-20 18:38:17 +0530

    Update dynamic_cast.cpp

--- dynamic_cast.cpp
+++ dynamic_cast.cpp
@@ -1,3 +1,9 @@
+//dynamic cast is used at run time to find out correct down-cast.
+// NOTE0: Need at least one virtual function in base class.
+// NOTE1: If the cast is successful, dynamic_cast returns a value of type new_type.
+// NOTE2: If the cast fails and new_type is a pointer type, it returns a null pointer of that type.
+// NOTE3: If the cast fails and new type is a reference type, it throws an exception that matches a handler of type std::bad_cast
+
 #include<iostream>
 using namespace std;
 
